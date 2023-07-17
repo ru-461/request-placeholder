942bd12f5be46aa39070b59533f45784b71a73bb
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-07-17 11:56:16 +0900

    chore: Create constant

---
+++ lib/constant/url.dart
@@ -0,0 +1,8 @@
+class URL {
+  static String todos = 'https://jsonplaceholder.typicode.com/todos';
+  static String posts = 'https://jsonplaceholder.typicode.com/posts';
+  static String comments = 'https://jsonplaceholder.typicode.com/albums';
+  static String albums = 'https://jsonplaceholder.typicode.com/photos';
+  static String photos = 'https://jsonplaceholder.typicode.com/todos';
+  static String users = 'https://jsonplaceholder.typicode.com/users';
+}
