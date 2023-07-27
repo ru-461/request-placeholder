5647a9e2dbfa29ab86b1f8dbed6bdd1b0ebf5c87
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-07-27 21:05:02 +0900

    update: Wrap ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:request_placeholder/app.dart';
 
-void main() => runApp(const App());
+void main() => runApp(const ProviderScope(child: App()));
