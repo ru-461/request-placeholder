a1b8a0a36dfba3765ab90c4b2a8d0bac474b94ec
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-12-24 00:35:59 +0900

    chore: Remove index provider

--- lib/providers/selected_Index_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'selected_index_provider.g.dart';
-
-@riverpod
-int selectedIndex(SelectedIndexProviderRef ref) {
-  return 1;
-}
