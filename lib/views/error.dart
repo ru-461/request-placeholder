3ee6af19f9c8cb9060f302a41092c6d7b5cdb786
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-12-26 01:25:30 +0900

    update: Add back home button

--- lib/views/error.dart
+++ lib/views/error.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Error extends StatelessWidget {
   const Error({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Page not found.')));
+    return Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Page not found.'),
+        ElevatedButton(
+            onPressed: () => context.go('/'), child: const Text('Home'))
+      ],
+    )));
   }
 }
